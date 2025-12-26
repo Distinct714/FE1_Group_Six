5504eeda80f5408554eefacd2fb315d7e81985b0
Distinct <distinct3579@gmail.com>
2025-12-26 20:18:40 +0800

    Update main.cpp

--- FE1_Final_Exam/main.cpp
+++ FE1_Final_Exam/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// class below with 10 features
+class Health_Check {
+    // For the creation of features, make it short and simple :3
+
+};
+
+// CLI Interaction
 int main() {
-    
+    // The block of code here is where it allows the user to choose the list of available features by entering a number from 0-9.
+    // This is where the while loop occurs to tell the user to continue if they want to check their health again or exit CLI.
 
     return 0;
 }
